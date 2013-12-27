c8fe7bcba585884040364986ea150ad0b19e68b4
dev@sokol-web.de <Kamill Sokol>
2013-12-27 12:21:01 +0100

    fixed unittest. added CustomExceptionResolverTest

--- minig/src/main/java/org/minig/server/resource/CustomExceptionResolver.java
+++ minig/src/main/java/org/minig/server/resource/CustomExceptionResolver.java
@@ -7,21 +7,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component("handlerExceptionResolver")
+@Component
 public class CustomExceptionResolver implements HandlerExceptionResolver {
 
     private static final Logger logger = Logger.getLogger(CustomExceptionResolver.class);
 
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object object, Exception exception) {
+        if(exception.getClass().getAnnotation(ResponseStatus.class) != null) {
+            return null;
+        }
+
+        //TODO naive approach. what about other exceptions?
+        if(exception.getClass().isAssignableFrom(MissingServletRequestParameterException.class)) {
+            return null;
+        }
+
         int status = 500;
 
-        Map<String, Object> data = new HashMap<String, Object>();
+        Map<String, Object> data = new HashMap<>();
         Object msg = null;
 
         if (exception != null) {
@@ -33,10 +46,12 @@ public class CustomExceptionResolver implements HandlerExceptionResolver {
             ObjectMapper objectMapper = new ObjectMapper();
 
             data.put("status", status);
-            if (msg != null) data.put("message", msg);
+            if (msg != null) {
+                data.put("message", msg);
+            }
 
             response.setStatus(status);
-            response.setContentType("application/json");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
             objectMapper.writeValue(response.getOutputStream(), data);
         } catch (Exception e) {
@@ -45,4 +60,5 @@ public class CustomExceptionResolver implements HandlerExceptionResolver {
 
         return new ModelAndView();
     }
+
 }
