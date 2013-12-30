1c9bbc60f4d142b857c927c2feb3e8c10c2a6964
dev@sokol-web.de <Kamill Sokol>
2013-12-30 19:10:01 +0100

    test should assert json structure not only array size

--- minig/src/test/js/test.js
+++ minig/src/test/js/test.js
@@ -1,23 +1,23 @@
 
 describe('FolderListCtrl', function(){
     var scope;//we'll use this scope in our tests
-    var $httpBackend;
+    var $httpBackend, fixtureFolderlist;
 
     beforeEach(module('minigApp', 'fixture/folderlist.json'));
 
     beforeEach(inject(function($rootScope, $controller, API_HOME, _$httpBackend_, _fixtureFolderlist_){
        $httpBackend = _$httpBackend_;
+       fixtureFolderlist = _fixtureFolderlist_;
+
        _$httpBackend_.whenGET(API_HOME+'folder').respond(_fixtureFolderlist_);
 
-        scope = $rootScope.$new();
+       scope = $rootScope.$new();
 
-        $controller('FolderListCtrl', {$scope: scope});
+       $controller('FolderListCtrl', {$scope: scope});
+       $httpBackend.flush();
     }));
 
-    it('scope should have folders.length == 5', function() {
-        expect(scope.folders.length).toBe(0);
-        $httpBackend.flush();
-        expect(scope.folders.length).toBe(5);
+    it('should have same json structure', function() {
+        expect(angular.toJson(scope.folders)).toEqual(angular.toJson(fixtureFolderlist.folderList));
     });
-
 });
