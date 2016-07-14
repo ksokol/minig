9095e365600d1e269224134cf88f78d578c2a7b3
Kamill Sokol <dev@sokol-web.de>
2016-07-14 22:06:59 +0200

    replaced spring.profiles.active JVM argument in spring-boot-maven-plugin with addResources

--- src/main/java/org/minig/config/MvcConfig.java
+++ src/main/java/org/minig/config/MvcConfig.java
@@ -1,10 +1,6 @@
 package org.minig.config;
 
-import org.apache.commons.lang3.SystemUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,27 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private Environment environment;
-
     @Override
     public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addViewController("login").setViewName("login.html");
         registry.addViewController("/").setViewName("index.html");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (environment.acceptsProfiles("dev")) {
-            final String userDir = environment.getProperty("user.dir");
-            String filePrefix = "file://";
-
-            if (SystemUtils.IS_OS_WINDOWS) {
-                filePrefix += "/";
-            }
-
-            registry.addResourceHandler("/**")
-                    .addResourceLocations(filePrefix + userDir + "/src/main/resources/static/");
-        }
     }
 }
